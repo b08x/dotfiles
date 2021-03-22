fd266f8050110e6e9b6a1b03cb090c694a569a94
b08x <rwpannick@gmail.com>
2021-03-22 06:21:56 -0400

    refactoring autostart

--- .config/i3/autostart.rb
+++ .config/i3/autostart.rb
@@ -1,49 +1,65 @@
 #!/usr/bin/env ruby
 
-require 'i3ipc'
+# require 'i3ipc'
+#
+# class I3
+#   attr_accessor :i3
+#
+#   def initialize
+#     @i3 = I3Ipc::Connection.new
+#   end
+#
+#   def command(cmd)
+#     command = @i3.command(cmd)
+#   end
+#
+# end
 
-class I3
-  attr_accessor :i3
-
-  def initialize
-    @i3 = I3Ipc::Connection.new
+def forkoff(command)
+  fork do
+    exec("#{command}")
   end
-
-  def command(cmd)
-    command = @i3.command(cmd)
-  end
-
 end
 
 host = `cat /etc/hostname`
-swallow = "/usr/local/bin/swallow"
-output = File.join(ENV["HOME"], '.screenlayout', '#{host}.sh')
-
 host = host.strip
+
+swallow = "/usr/local/bin/swallow"
+output = File.join(ENV["HOME"], '.screenlayout', host)
 
 case host
 when "soundbotMX"
-  system("synclient PalmDetect=1")
-  system("synclient PalmMinWidth=8")
-  system("synclient PalmMinZ=200")#
+  system("touchpad-indicator")
   system(output)
   system('nitrogen --restore')
-  sleep 2
-  system("#{swallow} -d polybar -r laptop")
+  sleep 1
+  fork do
+      exec("#{swallow} -d polybar -r laptop")
+  end
 when "ninjabot"
+  polybars = ["#{swallow} -d polybar -r landscape", "#{swallow} -d polybar -r portait"]
+
   system(output)
+
   system('nitrogen --restore')
-  sleep 2
-  system("#{swallow} -d polybar -r landscape")
-  system("#{swallow} -d polybar -r portait")
-  system("tilda")
+
+  sleep 1
+
+  polybars.each do
+    forkoff(polybar)
+  end
+
+  forkoff("tilda")
 end
 
-system("#{swallow} -d python3 $HOME/.config/i3/autotiling.py")
 
-system("ray_control open_session start")
+
+forkoff("#{swallow} -d python3 $HOME/.config/i3/autotiling.py")
+
+system("notify-send 'starting sound services'")
+forkoff("ray_control open_session start")
 sleep 1
-system("a2j_control start")
-system("pulseaudio --log-target=syslog --daemonize --high-priority --realtime --exit-idle-time=-1")
+forkoff("a2j_control start")
+forkoff("pulseaudio --log-target=syslog --daemonize --high-priority --realtime --exit-idle-time=-1")
 
-system("guake")
+forkoff("guake")
