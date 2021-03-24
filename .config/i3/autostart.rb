71bdd0513e6de021fe37fee6da545c0c84f35d70
b08x <rwpannick@gmail.com>
2021-03-24 13:24:11 -0400

    added qutebrowser configs

--- .config/i3/autostart.rb
+++ .config/i3/autostart.rb
@@ -1,19 +1,5 @@
-#!/usr/bin/env ruby
 
-# require 'i3ipc'
-#
-# class I3
-#   attr_accessor :i3
-#
-#   def initialize
-#     @i3 = I3Ipc::Connection.new
-#   end
-#
-#   def command(cmd)
-#     command = @i3.command(cmd)
-#   end
-#
-# end
+#!/usr/bin/env ruby
 
 def forkoff(command)
   fork do
@@ -21,44 +7,42 @@ def forkoff(command)
   end
 end
 
+# /usr/bin/laptop-detect
 host = `cat /etc/hostname`
 host = host.strip
 
 swallow = "/usr/local/bin/swallow"
+
 output = File.join(ENV["HOME"], '.screenlayout', host)
 
 blocks = File.join(ENV["HOME"], '.config', 'polybar', 'blocks', 'launch.sh')
+
 grayblocks = File.join(ENV["HOME"], '.config', 'polybar', 'blocks', 'launch.sh')
 
 
 if host == "soundbotMX"
   forkoff("touchpad-indicator &")
   forkoff(output)
-  # forkoff('nitrogen --restore')
   sleep 1
   forkoff("#{grayblocks}")
 end
 
-
-if "ninjabot"
-  forkoff(output)
-  # forkoff('nitrogen --restore')
-  sleep 1
-  #forkoff("#{polybar} --blocks")
-  # forkoff("tilda")
-  #forkoff("redshift -c '~/.config/redshift/redshift.conf' &")
-end
+forkoff(output)
 
 forkoff("#{swallow} -d python3 $HOME/.config/i3/autotiling.py")
+
 system("notify-send 'starting sound services'")
 forkoff("jack_control start")
 sleep 0.5
+
 system("notify-send 'starting raysession'")
 forkoff("ray_control open_session start")
 sleep 2
+
 system("notify-send 'starting a2jmidid'")
 forkoff("a2jmidid -e")
 sleep 1
+
 system("notify-send 'starting pulse'")
 forkoff("pulseaudio --log-target=syslog --daemonize --high-priority --realtime --exit-idle-time=-1")
 
@@ -66,5 +50,3 @@ forkoff("#{blocks}")
 forkoff("nitrogen --restore")
 
 forkoff("guake")
-
-
